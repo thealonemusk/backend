447b55661b62db27fcfdb5ea5100dc71e63c934b
Ashutosh Jha <105535224+thealonemusk@users.noreply.github.com>
2024-04-27 21:45:40 +0530

    enabled port binding

--- main.py
+++ main.py
@@ -182,6 +182,8 @@ async def dehaze_image(image: UploadFile = File(...)):
     except Exception as e:
         return {"error": str(e)}
 
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
